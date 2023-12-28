25dcfafbd911642ccdc53f99c2fe4c72b321a968
Nicole <nicatrombone@gmail.com>
2023-12-28 17:22:18 -0300

    New app Motivation

---
+++ app/src/main/java/com/nicnicdev/motivation/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.nicnicdev.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.nicnicdev.motivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNewSentece.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_sentece){
+         var s = ""
+        }
+    }
+}
---
+++ app/src/main/java/com/nicnicdev/motivation/UserActivity.kt
@@ -0,0 +1,30 @@
+package com.nicnicdev.motivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.nicnicdev.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityUserBinding
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityUserBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+
+        }
+
+    }
+}
