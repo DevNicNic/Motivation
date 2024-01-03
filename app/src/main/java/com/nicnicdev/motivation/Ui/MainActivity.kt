44a1d5f8741513833f8936734b9c41955ae30c5f
Nicole <nicatrombone@gmail.com>
2024-01-03 15:42:51 -0300

    Phrase button and phrase filter added.

--- app/src/main/java/com/nicnicdev/motivation/Ui/MainActivity.kt
+++ app/src/main/java/com/nicnicdev/motivation/Ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.nicnicdev.motivation.Ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.nicnicdev.motivation.Ui.Infra.MotivationConstants
 import com.nicnicdev.motivation.R
 import com.nicnicdev.motivation.Ui.Infra.SecurityPreferences
@@ -11,6 +12,7 @@ import com.nicnicdev.motivation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var categoryId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,16 +21,47 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         setContentView(binding.root)
 
         handleUserName()
+        handleFilter(R.id.image_all) // fingindo que estou clicando para que todos fiquem roxo
 
         binding.buttonNewSentece.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
 
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_sentece) {
             var s = ""
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            handleFilter(view.id)
+        }
+
+    }
+
+    private fun handleFilter(id: Int) {
+
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purble))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purble))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purble))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
+
         }
     }
+
     private fun handleUserName() {
         val name = SecurityPreferences(this).getString(MotivationConstants.KEY.USER_NAME)
         binding.textUserName.text = "Ola, $name!"
